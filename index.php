97f697849d4e567090232acf6f23020a325112ea
fostermorgan <43851125+cerealman200@users.noreply.github.com>
2019-07-02 13:52:50 -0500

    quote added, fixed  social/nav

--- index.php
+++ index.php
@@ -4,34 +4,137 @@
 <!DOCTYPE html>
 <html>
     <head>
-
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-        <link rel="stylesheet" type="text/css" href="main.css" />
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Just+Another+Hand" />
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Homemade+Apple" />
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <style >
+          .handwritten{
+            font-family: "Just Another Hand";
+            font-size: 90px;
+            font-weight: 0;
+            line-height: 100px;
+            color: black;
+            text-shadow: 7px 7px 0px rgba(0,0,0,0.1);
+          }
+          .logo{
+            font-family: serif;
+            font-style: italic;
+            font-size: 2em;
+          }
+          .navbar{
+            background-color: black;
+
+          }
+          .ivory{
+            color: ivory;
+          }
+          .bg-image {
+              position: relative;
+          }
+          .bg-image img {
+              display: block;
+              width: 100%;
+              background-size: cover;
+              margin: 0 auto;
+              filter: blur(5px);
+              -webkit-filter: blur(5px);
+              /* max-height: 100%; */
+          }
+          .bg-image h1 {
+              position: absolute;
+              text-align: center;
+              top: 15%;
+              bottom: 0;
+              left: 0;
+              right: 0;
+          }
+          .footer {
+            position: absolute;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            color: white;
+            /* background-color: ivory; */
+            text-align: center;
+          }
+          .nav, .main {
+              background-color: #f6f6f6;
+              text-align: center;
+          }
+          body{
+            background-color: black;
+          }
+
+          /*    social media buttons */
+          .fa {
+              padding: 20px;
+              font-size: 30px;
+              width: 70px;
+              text-align: center;
+              text-decoration: none;
+              margin: 5px 2px;
+              border-radius: 50%;
+          }
+
+          .fa:hover {
+              opacity: 0.4;
+              text-decoration: none;
+              color: ivory;
+          }
+
+          .fa-facebook {
+              background: #3B5998;
+              color: white;
+          }
+
+          .fa-twitter {
+              background: #55ACEE;
+              color: white;
+          }
+          .fa-mail-reply {
+              background: #ff3f3a;
+              color: white;
+          }
+          .fa-instagram {
+              background: #df00db;
+              color: white;
+          }
+          .fa-snapchat-ghost {
+              background: #fffc00;
+              color: white;
+              text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+          }
+          /* .container-fluid{
+            padding-left: 250px;
+          } */
+
+
+        </style>
 
         <title>K&M Hot Sauce Co.</title>
-
     </head>
     <body>
-        <!-- <div class="col text-center headquote">
-            K & M Hot Sauce Co.
-        </div> -->
 
 
-
-        <?php include('nav.php'); ?>
-        <div id="bg" class="container-fluid pr-0 pl-0">
-
+        <div class="container-fluid pr-0 pl-0">
+                  <?php include 'nav.php'; ?>
 
 
-          <div class="footer pb-3 pt-3">
-            <a href="#" class="fa fa-facebook"></a>
-            <a href="#" class="fa fa-twitter"></a>
-            <a href="#" class="fa fa-instagram"></a>
-            <a href="#" class="fa fa-snapchat-ghost"></a>
-            <a href="mailto:wiscoville@gmail.com" class="fa fa-mail-reply"></a>
-          </div>
+                  <div class="bg-image col-12 pr-0 pl-0">
+                      <img src="fulldrawn.jpg">
+                      <h1 class="handwritten">Hand made in Eau Claire, Wisconsin</h1>
+                      <div class="footer pb-3 pt-5">
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-instagram"></a>
+                        <a href="#" class="fa fa-snapchat-ghost"></a>
+                        <a href="mailto:wiscoville@gmail.com" class="fa fa-mail-reply"></a>
+                      </div>
+                  </div>
         </div>
+
 
 
 
--- nav.php
+++ nav.php
@@ -1,24 +1,22 @@
-<nav class="navbar navbar-expand-lg navbar-dark " id="topbar">
-    <a class="navbar-brand headquote" href="index.php">K & M Hot Sauce Co.</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-                <li class="nav-item <?php if($page === "report"){ echo 'active';}?>">
-                    <a class="nav-link" href="#about">About</a>
-                </li>
-            <li class="nav-item <?php if($page === "hunts"){ echo 'active';}?>">
-                <a class="nav-link" href="contact.php">Contact</a>
-            </li>
-        </ul>
+<nav class="navbar navbar-expand-lg navbar-dark container">
+  <a class="navbar-brand logo" href="index.php">K & M Hot Sauce Co.</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse w-100 order-3 dual-collapse2" id="navbarNavDropdown">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle ivory" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          About
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">The Sauce</a>
+          <a class="dropdown-item" href="#">The Creators</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
 </nav>
-
-<!-- <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197"> -->
-  <!-- <ul class="nav navbar-nav">
-    <li class="active headquote"><a href="index.php">K & M Hot Sauce Co.</a></li>
-    <li><a href="#">Page 1</a></li>
-    <li><a href="#">Page 2</a></li>
-    <li><a href="#">Page 3</a></li>
-  </ul>
-</nav> -->
