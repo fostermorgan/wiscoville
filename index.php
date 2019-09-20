484e604c76fa034e1fbc6739ed259ba2b4d5465d
Levi Cavagnetto <lpcavagnetto@gmail.com>
2019-09-20 17:30:54 -0500

    fixed newsletter email bar
    email bar now stays within the page on mobile mode

--- index.php
+++ index.php
@@ -257,7 +257,7 @@
 
         </div>
         <!-- newsletter -->
-      <div class="newsletter col-12 col-lg-6 text-center ml-0 row">
+      <div class="newsletter col-12 col-lg-6 text-center m-0 row">
         <form id="newsletterform" action="#" class="mt-0 mb-2" method="post" name="newsletter">
           <div class="newsletterheader text-center">
              <p>Newsletter</p>
@@ -267,31 +267,12 @@
           <div class="newsletterdescription">
             <p>K&M Sauce Co. is almost ready to go live. If you're interested, then sign up below to be the first to know when we are.</p>
           </div>
-          <div class="input">
-            <input type="text" class="button" id="newsletteremail" name="newsletteremail" placeholder="NAME@EXAMPLE.COM" required>
-            <input type="submit" class="button" id="newsletterSubmit" name="newsletterSubmit" value="SIGN UP">
+          <div class="input row m-0">
+            <input type="text" class="button col-8" id="newsletteremail" name="newsletteremail" placeholder="NAME@EXAMPLE.COM" required>
+            <input type="submit" class="button col-4" id="newsletterSubmit" name="newsletterSubmit" value="SIGN UP">
           </div>
         </form>
       </div>
-
-      <!-- <div class="newsletter col-12 d-lg-none border border-dark text-center row">
-        <form id="newsletterform" action="#" class="mt-0 mb-2" method="post" name="newsletter">
-          <div class="newsletterheader">
-             <p>Newsletter</p>
-          </div>
-          <hr class="divider my-0 pb-4">
-
-          <div class="newsletterdescription">
-            <p>K&M Sauce Co. is almost ready to go live. If you're interested, then sign up below to be the first to know when we are.</p>
-          </div>
-          <div class="input">
-            <input type="text" class="button" id="newsletteremail" name="newsletteremail" placeholder="NAME@EXAMPLE.COM" required>
-            <input type="submit" class="button" id="newsletterSubmit" name="newsletterSubmit" value="SIGN UP">
-          </div>
-        </form>
-      </div> -->
-
-
 
     </div>
     <!-- Copyright -->
